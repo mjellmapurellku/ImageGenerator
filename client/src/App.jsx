5e128ba423430c26e49e41baa35f16b544ca274a
Benina <benina.sharapolli@gmail.com>
2025-05-30 17:16:33 +0200

    new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,34 @@
-import React, { useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import BuyCredit from './pages/BuyCredit'
-import Home from './pages/Home'
-import Result from './pages/Result'
-import Login from './components/Login'
-import { AppContext } from './context/AppContext'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-function App(){
+function App() {
+  const [count, setCount] = useState(0)
 
-  const {showLogin} = useContext(AppContext)
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
-
-    <Navbar/>
-    {showLogin && <Login/>}
-      <Routes>
-        <Route path='/' element={ <Home/>}/>
-        <Route path='/result' element={ <Result/>}/>
-        <Route path='/buy' element={ <BuyCredit/>}/> 
-      </Routes>
-      <Footer/>
-      
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
